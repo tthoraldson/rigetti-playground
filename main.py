0d6fc075aff868c32f562ea15e8b69569bb3db67
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2018-06-07 22:51:41 -0500

    hello, world

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Hello, World!
+
+from pyquil.quil import Program
+from pyquil.gates import H, CNOT
+from pyquil.api import SyncConnection
+# construct a Bell State program
+p = Program()
+p.inst(H(0))
+p.inst(CNOT(0, 1))
+# run the program on a QVM
+qvm = SyncConnection()
+result = qvm.wavefunction(p)
